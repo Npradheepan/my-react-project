d1592ce16f93a0f875a38c64f9827284ac935b40
pradh <pradh@DESKTOP-765MFUR>
2022-05-26 19:30:19 +0200

    thirdlevel

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -20,44 +20,48 @@ const Content = () => {
         }
     ]);
 
-    const handleCheck = (id) => {
-        const listItems = items.map((item) => item.id === id ? { ...item, checked: ! item.checked } : item);
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    const setAndSaveItem = (newLists) => {
+        setItems(newLists)
+        localStorage.setItems("shoppingList", JSON.stringify(newLists))
+    }
+    // Second methode to handle Check
+    const  handleCheck = (id) => {
+        const listItem = items.map((item) => item.id==id ? {...item, checked: !item.checked}:item)
+        // setItems(listItem)
+        // localStorage.setItems("shoppingList", JSON.stringify(listItem));
+        setAndSaveItem(listItem)
+    }
+    // Second methode to handle Delete
+    const handleDelete = (id) => {
+       const listItem =  items.filter((item)=>item.id !== id)
+        setAndSaveItem(listItem);
     }
 
-    const handleDelete = (id) => {
-        const listItems = items.filter((item) => item.id !== id);
-        setItems(listItems);
-        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    }
 
     return (
+       
         <main>
-            {items.length ? (
                 <ul>
                     {items.map((item) => (
                         <li className="item" key={item.id}>
                             <input
-                                type="checkbox"
                                 onChange={() => handleCheck(item.id)}
+                                type="checkbox"
                                 checked={item.checked}
                             />
                             <label
                                 style={(item.checked) ? { textDecoration: 'line-through' } : null}
-                                onDoubleClick={() => handleCheck(item.id)}
-                            >{item.item}</label>
+                                onDoubleClick = {() => handleCheck(item.id)}
+                            >{item.item}
+                            </label>
                             <FaTrashAlt
-                                onClick={() => handleDelete(item.id)}
                                 role="button"
                                 tabIndex="0"
+                                onClick={()=>handleDelete(item.id)}
                             />
                         </li>
                     ))}
                 </ul>
-            ) : (
-                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-            )}
         </main>
     )
 }
