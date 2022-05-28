ca6a6fc389fa9a5da4bb25bc4809c8aa3a77f5a0
pradh <pradh@DESKTOP-765MFUR>
2022-05-28 05:46:26 +0200

    thirddayfirstlevel

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -5,7 +5,6 @@ const ItemList = ({ items, handleCheck, handleDelete }) => {
         <ul>
             {items.map((item) => (
                 <LineItem
-            
                     key={item.id}
                     item={item}
                      handleCheck={handleCheck}
