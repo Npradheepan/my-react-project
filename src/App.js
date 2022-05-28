787f583764df7ff1ff79f59e96dbe3daa8f6b234
pradh <pradh@DESKTOP-765MFUR>
2022-05-28 14:28:53 +0200

    thirddayfivthlevel

--- src/App.js
+++ src/App.js
@@ -9,22 +9,29 @@ function App() {
   const API_URL = 'http://localhost:3500/posts'
 
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // localStorage.setItem('shoppinglist', JSON.stringify(items));
+
     const fetchItems = async () => {
-        try {
-          const response = await fetch(API_URL);
-          const listItems = await response.json();
-          console.log(listItems)
-          setItems(listItems)              
-        }catch(err) {
-          console.log(err.stack)
-        }
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
-    (async()=> await fetchItems ())()
+
+    setTimeout(() => fetchItems(), 2000);
+
   }, [])
 
   const addItem = (item) => {
@@ -63,11 +70,15 @@ function App() {
         search={search}
         setSearch={setSearch}
       /> 
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+       <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
